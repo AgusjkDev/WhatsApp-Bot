01c2f49d04191c5a332ce03ad92177e1cef013c8
AgusjkDev <agusjkdev7@gmail.com>
2023-01-18 16:51:21 -0300

    Release version 0.1.1 with headless argument

--- src/constants.py
+++ src/constants.py
@@ -3,11 +3,12 @@ from dotenv import dotenv_values
 
 environ = dotenv_values(".env")
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 BRAVE_PATH = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application"
 DRIVER_ARGUMENTS = [
     "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
     "--mute-audio",
+    "--headless",
     "--disable-extensions",
     "--disable-dev-sh-usage",
     "--disable-gpu",
